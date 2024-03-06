c69dba3330af7f63323962e194f00776891c8d13
Prajwal Pawar <114561900+PrajwalPawar789@users.noreply.github.com>
2024-03-06 19:17:37 +0530

    added pricing page

--- src/App.js
+++ src/App.js
@@ -3,12 +3,17 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginPage from './LoginPage';
 import SearchPage from './SearchPage';
 import PrivateRoutes from './PrivateRoutes';
+import PricingPage from './PricingPage';
+import SignUpForm from './SignUpForm';
 
 const App = () => {
     return (
         <Router>
             <Routes>
                 <Route path="/login" element={<LoginPage />} />
+                <Route path="/pricingpage" element={<PricingPage/>} />
+                <Route path="/signup" element={<SignUpForm/>} />
+
                 <Route element={<PrivateRoutes />}>
                     <Route path="/search" element={<SearchPage />} />
                 </Route>
